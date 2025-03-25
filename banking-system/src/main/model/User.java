e5c119d83d2d7e4c638ff802011c414cc536a58a
sahithya-28 <tsahithya28@gmail.com>
2025-03-25 13:24:18 +0530

    Add files via upload

---
+++ banking-system/src/main/model/User.java
@@ -0,0 +1,20 @@
+package com.ex.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String email;
+    private String password; // Store hashed password
+    private String role;
+}
